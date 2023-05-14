f72a33173facb6944df3b8076a6d5453c3d4632d
Jorge <jorge.arancibia.jeal@gmail.com>
2023-05-14 19:57:52 -0400

    base del proyecto con el header y avatar listo

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import './avatar.css'
+
+const Header = () => {
+  return (
+    <div className="padre">
+        <div className="avatar" />
+        <h1 className="texto">Jorge Eduardo <p> Arancibia Leiva</p></h1>
+    </div>
+  )
+}
+
+export default Header;
