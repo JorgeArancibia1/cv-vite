9a5daf1c494479c83b0f2a6525a9d68d76721a05
Jorge <jorge.arancibia.jeal@gmail.com>
2023-12-31 21:44:55 -0300

    Avance

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import "./index.css";
+
+export const Avatar = () => {
+	return <div className='avatar'></div>;
+};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,19 @@
+import { Avatar } from "../Avatar";
+import "./index.css";
 
 const Header = () => {
-  return (
-    <div className="padre">
-        <div className="avatar" />
-        <h1 className="texto">Jorge Eduardo <p> Arancibia Leiva</p></h1>
-    </div>
-  )
-}
+	return (
+		<section className='container-fluid m-auto p-0'>
+			<div className='avatar-container'>
+				<Avatar />
+			</div>
+			<div className="principal-text-container">
+				<h1 className='texto'>
+					Jorge Eduardo <p> Arancibia Leiva</p>
+				</h1>
+			</div>
+		</section>
+	);
+};
 
 export default Header;
