ff904de5703b479f169cbe752a1740d7359f12fd
Jorge <jorge.arancibia.jeal@gmail.com>
2024-01-19 04:24:20 -0300

    .

--- src/config/i18n.config.ts
+++ src/config/i18n.config.ts
@@ -1,6 +1,8 @@
 import i18next from 'i18next';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
+import translationEN from '../../public/locales/en/translation.json';
+import translationES from '../../public/locales/es/translation.json';
 
 i18next
 	.use(Backend)
@@ -13,5 +15,13 @@ i18next
 		},
 		backend: {
 			loadPath: 'public/locales/{{lng}}/translation.json',
+		},
+		resources:{
+			en:{
+				translation:translationEN,
+			},
+			es:{
+				translation:translationES,
+			}
 		},
 	});
